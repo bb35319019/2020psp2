251eaa2e5b8362be0e9fdecd09e6000ef80ae0d8
kimucchi <shota0801.k@icloud.com>
2020-07-22 21:00:01 +0900

    変数の代入。

--- k04/k04.c
+++ k04/k04.c
@@ -1,1 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
 
+struct DATE
+{
+    int ID;
+    int gender;
+    double heights;
+};
+int main(void){
+    char fname[FILENAME_MAX];
+    
+}
