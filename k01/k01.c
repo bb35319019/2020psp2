297ca9409507002290307f9512caff3be773bd4d
kimucchi <shota0801.k@icloud.com>
2020-08-03 00:47:26 +0900

    プログラムの修正と説明

--- k01/k01.c
+++ k01/k01.c
@@ -2,48 +2,40 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-double n;
-extern double ave_online(double val,double ave);
-extern double var_online(double val,double ave,double ave2);
-int main(void)
-{
-    double val,ave=0,var=0,ave2=0,s_var;
+int main(void){
+    double val,ave=0,std_err,average,var,Suitei_average,Suitei_var,ave2=0;
     char fname[FILENAME_MAX];
     char buf[256];
     FILE* fp;
-
+    int n;
+    n=0;
     printf("input the filename of sample:");
     fgets(fname,sizeof(fname),stdin);
     fname[strlen(fname)-1] = '\0';
     printf("the filename of sample: %s\n",fname);
-
     fp = fopen(fname,"r");
     if(fp==NULL){
         fputs("File open error\n",stderr);
         exit(EXIT_FAILURE);
     }
-
-    while(fgets(buf,sizeof(buf),fp) != NULL){
+    while(fgets(buf,sizeof(buf),fp)!=NULL){
         sscanf(buf,"%lf",&val);
-        n=n+1;
-        ave=ave_online(val,ave);
-        var=var_online(var,ave,ave2);
-        ave2=ave_online(pow(val,2),ave2);
+        n++;
+        ave=((n-1)*ave+val)/n;
+        ave2=((n-1)*ave2+val*val)/n;
+        var=ave2-ave*ave;
     }
-    
-    if(fclose(fp) == EOF){
+    average=ave;
+    Suitei_average=average;
+    Suitei_var=n*var/(n-1);
+    std_err=pow(Suitei_var/n,0.5);
+    if(fclose(fp)==EOF){
         fputs("file close error\n",stderr);
         exit(EXIT_FAILURE);
     }
-     printf("%f\n,n");
-     s_var=(n/(n-1))*var;
-     printf("Average:%f\n",ave);
-     printf("Var:%f\n",var);
-     printf("Suitei_Average:%f\n",ave);
-     printf("Suitei_Var:%f\n",s_var);
-
+    printf("Average=%.2lf\n",average);
+    printf("Var=%.2lf\n",var);
+    printf("Suitei_average=%.2lf,std_err=%.2lf\n",average,std_err);
+    printf("Suitei_var=%.2lf\n",Suitei_var);
     return 0;
-
-
 }
-
