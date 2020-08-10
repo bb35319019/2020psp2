f2efd91dada876ceea3b733bd67e074d4938d403
kimucchi <shota0801.k@icloud.com>
2020-08-10 20:42:16 +0900

    関数の変更

--- k01/k01.c
+++ k01/k01.c
@@ -2,8 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+  double ave_online(double val, double ave_old, int n)
+  {
+    double ave_new;
+    ave_new = (n-1)*ave_old + val;
+    ave_new = ave_new/n;
+    return ave_new;
+  }
 int main(void){
-    double val,ave=0,std_err,average,var,ave_online,var_online,ave2=0;
+    double val,ave=0,std_err,average,var,var_online,ave2=0,ave__online,Z;
     char fname[FILENAME_MAX];
     char buf[256];
     FILE* fp;
@@ -21,12 +28,12 @@ int main(void){
     while(fgets(buf,sizeof(buf),fp)!=NULL){
         sscanf(buf,"%lf",&val);
         n++;
-        ave=((n-1)*ave+val)/n;
-        ave2=((n-1)*ave2+val*val)/n;
-        var=ave2-ave*ave;
+        ave=ave_online(val,ave,n);
+        ave2=ave_online(pow(val,2),ave2,n);
+        var=ave2-pow(ave,2);
     }
     average=ave;
-    ave_online=average;
+    ave__online=average;
     var_online=n*var/(n-1);
     std_err=pow(var_online/n,0.5);
     if(fclose(fp)==EOF){
